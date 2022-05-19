e32fe99ff3172e48599680b25d992023f68a3c80
chanmi910 <chanmi910@gmail.com>
2022-05-19 01:33:08 +0000

    pr: pr test

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-	System.out.println("aaaaaaabbbbbbbbba");
+	System.out.println("aaaaaaabbbbbbbbcccccba");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
