4dd6bf7773923d19421c67579718569fe3539972
chanmi910 <90135441+chanmi910@users.noreply.github.com>
2022-05-20 16:32:24 +0900

    Update variables.sh

--- .jx/variables.sh
+++ .jx/variables.sh
@@ -1,4 +1,4 @@
 export JX_PREVIEW_SERVICE="preview-svc"
 export JX_PREVIEW_PATH="/preview"
-export MINK_IMAGE='docker.io/chanmi910/jx3-spring-demo:0.1'
-export VERSION='0.1'
+#export MINK_IMAGE='docker.io/chanmi910/jx3-spring-demo:0.1'
+#export VERSION='0.1'
