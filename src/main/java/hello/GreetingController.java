71e8813ab3f578a8d5778c04b498271685aa080d
chanmi910 <90135441+chanmi910@users.noreply.github.com>
2022-05-20 11:10:14 +0900

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-	System.out.println("avasdasdasdaaaabbbbbbbbcccc12321cba");
+	System.out.println("avasdasdasdaaaabbbbbb2321cba");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
